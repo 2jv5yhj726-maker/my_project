01c6a933be3e8402c58b4c011eec57165bb5c65d
2jv5yhj726-maker <2jv5yhj726@privaterelay.appleid.com>
2026-02-08 09:47:05 +0300

    Update my_project_2.py

--- my_project_2.py
+++ my_project_2.py
@@ -34,7 +34,7 @@ URL = os.getenv("URL", "https://tce.by/search.html")
 
 EXPECTED_COUNT_1 = 4
 EXPECTED_COUNT_2 = 5
-EXPECTED_COUNT_3 = 4
+EXPECTED_COUNT_3 = 3
 
 
 # ============================================================
@@ -195,6 +195,7 @@ def main_once():
 
 if __name__ == "__main__":
     main_once()
+
 
 
 
